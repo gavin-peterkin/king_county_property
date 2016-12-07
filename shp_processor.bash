73e42289abf549e44a5a3cb5557c5b5ad4da5fe7
gavin-peterkin <glpeterkin@me.com>
2016-12-06 17:50:37 -0800

    general updates and removing old sql query file

--- shp_processor.bash
+++ shp_processor.bash
@@ -3,11 +3,21 @@
 # Iterate through *.shp files and send to the database
 # Makes use of 'postgis'
 
-for shp_file in `ls ./data/gis/property/*.shp`; do
+for shp_file in `ls ./data/gis/*/*.shp`; do
     TABLE_NAME="$(basename "$shp_file" .shp)"
     echo -n "$TABLE_NAME"
     # SRID: 2926
     # according to http://prj2epsg.org/search
-    shp2pgsql -s 2926 "$shp_file" gis."$TABLE_NAME" >> "./data/gis/property/$TABLE_NAME.sql"
-    cat "./data/gis/property/$TABLE_NAME.sql" | psql -h "handelstaccato.homenet.org" -p 5432 -U gavin king_county
+    shp2pgsql -s 2926 "$shp_file" gis."$TABLE_NAME" >> "./data/gis/$TABLE_NAME.sql"
+    # cat "./data/gis/$TABLE_NAME.sql" | psql -h "handelstaccato.homenet.org" -p 5432 -U gavin king_county
 done
+
+
+# for shp_file in `ls ./data/gis/property/*.shp`; do
+#     TABLE_NAME="$(basename "$shp_file" .shp)"
+#     echo -n "$TABLE_NAME"
+#     # SRID: 2926
+#     # according to http://prj2epsg.org/search
+#     shp2pgsql -s 2926 "$shp_file" gis."$TABLE_NAME" >> "./data/gis/property/$TABLE_NAME.sql"
+#     cat "./data/gis/property/$TABLE_NAME.sql" | psql -h "handelstaccato.homenet.org" -p 5432 -U gavin king_county
+# done
